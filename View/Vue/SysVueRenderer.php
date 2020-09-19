1c9f5e2a1c30fa2ae4c143458da29d8b7a549b84
opoink <37372895+opoink@users.noreply.github.com>
2020-09-19 10:59:12 +0800

    cache the sys vue components

--- View/Vue/SysVueRenderer.php
+++ View/Vue/SysVueRenderer.php
@@ -9,11 +9,41 @@ use MatthiasMullie\Minify;
 
 class SysVueRenderer {
 
-
+	const REWRITEVUECOMPONENTJS = true;
 	const COMPONENT_DIR = ROOT . DS . 'vendor'.DS.'opoink'.DS.'framework'.DS.'View'.DS.'Sys'.DS.'vue'.DS.'components';
 	const SERVICE_DIR = ROOT . DS . 'vendor'.DS.'opoink'.DS.'framework'.DS.'View'.DS.'Sys'.DS.'vue'.DS.'services';
 	protected $components = [];
 	protected $services = [];
+	protected $_fileWriter;
+
+	/** this will be the location of the vue compent taht will be generated */
+	protected $targetDir = '';
+	protected $targetFilename = '';
+	protected $targetFilenameExt = '';
+
+	public function __construct(
+		\Of\File\Writer $Writer
+	){
+		$this->_fileWriter = $Writer;
+		$this->targetDir = ROOT . DS . 'Var' . DS . 'Sys' . DS . 'Js';
+		$this->targetFilename = 'sysvuecomponents';
+		$this->targetFilenameExt = 'js';
+	}
+
+	/**
+	 * check if the vue js component is already exist
+	 * return boolean
+	 */
+	private function isExist(){
+		if(self::REWRITEVUECOMPONENTJS){
+			return false;
+		} else {
+			return true;
+		}
+		$curentDir = dirname(dirname(__FILE__)) . DS . 'Sys' . DS . 'Js' . DS . 'sysvuecomponents.js';
+		var_dump($curentDir);
+		die;
+	}
 
 	/**
 	 * get all components for systems ui
@@ -22,6 +52,17 @@ class SysVueRenderer {
 	 * in systems UI
 	 */
 	public function getComponents(){
+		$target = $this->targetDir . DS . $this->targetFilename . '.' . $this->targetFilenameExt;
+		if(!self::REWRITEVUECOMPONENTJS && !file_exists($target)){
+			$this->getComponentsHelper();
+		}
+		elseif(self::REWRITEVUECOMPONENTJS) {
+			$this->getComponentsHelper();
+		}
+		return $this;
+	}
+
+	protected function getComponentsHelper(){
 		$this->getSirvices();
 
 		$components = scandir(self::COMPONENT_DIR);
@@ -31,7 +72,6 @@ class SysVueRenderer {
 			}
 			$this->setComponent($componentName);
 		}
-		return $this;
 	}
 
 	/**
@@ -131,6 +171,13 @@ class SysVueRenderer {
 	 * and return minified js content
 	 */
 	public function toJs(){
+		$target = $this->targetDir . DS . $this->targetFilename . '.' . $this->targetFilenameExt;
+
+		if(!self::REWRITEVUECOMPONENTJS){
+			if(file_exists($target)){
+				return file_get_contents($target);
+			}
+		}
 		$minifier = new Minify\JS();
 
 		$Vue = '';
@@ -154,24 +201,31 @@ class SysVueRenderer {
 				".$data."
 			},
 			beforeMount(){
-		    },
-		    mounted(){
-		    	let vCheck = setInterval(f => {
-		    		if(_vue){
+			},
+			mounted(){
+				let vCheck = setInterval(f => {
+					if(_vue){
 						let page = this.router.init();
 						if(typeof this[page] != 'undefined'){
 							if(typeof this[page].init === 'function') {
 								this[page].init();
 							}
 						}
-	    				clearInterval(vCheck);
-		    		}
-	    		}, 100)
-		    }
+						clearInterval(vCheck);
+					}
+				}, 100)
+			}
 		})";
 		$minifier->add($Vue);
 
 		$js = $minifier->minify();
+
+		$this->_fileWriter->setDirPath($this->targetDir)
+		->setData($js)
+		->setFilename($this->targetFilename)
+		->setFileextension($this->targetFilenameExt)
+		->write();
+
 		return $js;
 	}
 }
