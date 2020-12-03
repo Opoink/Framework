70ed676bddc5808e5b7a8de0d67d1b240cfc4f65
opoink <37372895+opoink@users.noreply.github.com>
2020-12-03 20:16:01 +0800

    join table, removeData,
    in join sql query builder add the 4th param to idetify if the the 2nd field was a string or a table field.
    removeData method the key became optional, if it is null then it will remove all set index in data array.

--- Database/Sql/Statements/JoinTable.php
+++ Database/Sql/Statements/JoinTable.php
@@ -23,6 +23,41 @@ class JoinTable Extends \Of\Database\Sql\Statements\Statement {
 	protected $joinTmp = '';
 
 	/**
+	 * unsecure value for join will be added here
+	 */
+	public $unsecureValue = [];
+
+	/**
+     * current value for variable of value sample :a 
+     */
+    public $valVar = 'a';
+
+    /**
+     * prefix that will be appended on query for insecure data
+     */
+    public $valPrefix = 'join_';
+
+    /**
+     * return the new unsecure key
+     */
+    public function getUnsecureKey(){
+    	$key = ":".$this->valPrefix.$this->valVar.'opoink';
+    	$this->valVar++;
+    	return $key;
+    }
+
+    /**
+     * set the unsecure value
+     * return string unsecure key
+     * @param $value string
+     */
+    public function setUnsecureValue($value){
+    	$key = $this->getUnsecureKey();
+		$this->unsecureValue[$key] = $value;
+		return $key;
+    }
+
+	/**
      * @param $tableName the table name to add in query as join
      * @param $alias string
      * @param $joinType either INNER, LEFT, RIGHT, 'LEFT OUTER', 'RIGHT OUTER'
@@ -41,37 +76,77 @@ class JoinTable Extends \Of\Database\Sql\Statements\Statement {
 		return $this;
 	}
 
-	public function onJoin($field1, $field2, $condition="="){
-		$this->joinHelper(self::OPON, $field1, $field2, $condition);
+	public function onJoin($field1, $field2, $condition="=", $field2isString=false){
+		if(!$field2isString){
+			$f2 = $this->parseStr($field2);
+		} else {
+			$f2 = $this->setUnsecureValue($field2);
+		}
+		$this->joins[] = [
+			'table' => $this->joinTmp,
+			'fields' => [
+				[
+					'type' => self::OPON,
+					'field' => $this->parseStr($field1) . ' '.$condition.' '.$f2
+				]
+			]
+		];
+
 		$this->joinTmp = '';
 		return $this;
 	}
 
-	public function andJoin($field1, $field2, $condition="="){
+	public function andJoin($field1, $field2, $condition="=", $field2isString=false){
+		if(!$field2isString){
+			$f2 = $this->parseStr($field2);
+		} else {
+			$f2 = $this->setUnsecureValue($field2);
+		}
+
 		$lastKey = array_key_last($this->joins);
 
 		$this->joins[$lastKey]['fields'][] = [
 			'type' => self::OPAND,
-			'field' => $this->parseStr($field1) . ' '.$condition.' ' . $this->parseStr($field2)
+			'field' => $this->parseStr($field1) . ' '.$condition.' ' . $f2
 		];
 		return $this;
 	}
 
-	public function orJoin($field1, $field2, $condition="="){
+	public function orJoin($field1, $field2, $condition="=", $field2isString=false){
+		if(!$field2isString){
+			$f2 = $this->parseStr($field2);
+		} else {
+			$f2 = $this->setUnsecureValue($field2);
+		}
+
 		$lastKey = array_key_last($this->joins);
 
 		$this->joins[$lastKey]['fields'][] = [
 			'type' => self::OPOR,
-			'field' => $this->parseStr($field1) . ' '.$condition.' ' . $this->parseStr($field2)
+			'field' => $this->parseStr($field1) . ' '.$condition.' ' . $f2
 		];
 		return $this;
 	}
 
 	public function joinGroup($values){
 		if($values instanceof \Closure){
+         	if($this->valPrefix == 'join_'){
+	            $this->valPrefix = 'join_a';
+	        } else {
+	            $this->valPrefix++;
+	        }
 			$di = new \Of\Std\Di();
         	$subJoin = $di->get('\Of\Database\Sql\Statements\JoinTable');
+	        $subJoin->valPrefix = $this->valPrefix;
+	        $this->valPrefix++;
+	        $this->valPrefix++;
+
             $values($subJoin);
+
+           	foreach ($subJoin->unsecureValue as $key => $value) {
+           		$this->unsecureValue[$key] = $value;
+           	}
+
             $lastKey = array_key_last($this->joins);
             
            	if(!is_integer($lastKey)){
@@ -93,17 +168,24 @@ class JoinTable Extends \Of\Database\Sql\Statements\Statement {
 		}
 	}
 
-	public function joinHelper($operator, $field1, $field2, $condition="="){
+	/**
+	 * this method was not been used
+	 */
+	/*public function joinHelper($operator, $field1, $field2, $condition="=", $field2isString=false){
+		$f2 = "'".$field2."'";
+		if(!$field2isString){
+			$f2 = $this->parseStr($field2);
+		}
 		$this->joins[] = [
 			'table' => $this->joinTmp,
 			'fields' => [
 				[
 					'type' => self::OPON,
-					'field' => $this->parseStr($field1) . ' '.$condition.' ' . $this->parseStr($field2)
+					'field' => $this->parseStr($field1) . ' '.$condition.' ' . $f2
 				]
 			]
 		];
-	}
+	}*/
 
 	public function getJoinQry($isSub=false){
 		$qry = "";
