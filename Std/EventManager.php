097deede5a8df68ec1afdc0f750f59c984bc1f6c
opoink <ronromeperez@yahoo.com>
2022-06-26 06:29:13 +0000

    in controller add method getDi to get the dipendency injector class in phtml template, start of the enent manager

---
+++ Std/EventManager.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* Copyright 2018 Opoink Framework (http://opoink.com/)
+* Licensed under MIT, see LICENSE.md
+*
+* this DataObject is like the Magento 2 \Magento\Framework\DataObject
+* since the idea came from it
+*/
+namespace Of\Std;
+
+class EventManager extends DataObject {
+
+	/**
+	 * set the event that will be called from other classes
+	 */
+	public function setEvent($name){
+
+	}
+
+	public function runEvent($name, $param){
+
+	}
+}
+?>
